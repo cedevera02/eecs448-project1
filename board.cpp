16196559f22e87ce8c9c5b55bd1915fec5a23f7d
RobDog04 <78656529+RobDog04@users.noreply.github.com>
2021-02-17 00:32:22 -0600

    Board Class Updates
    We got in a group call and laid down the basis for three different functions. board, drawshipboard and drawFullBoard. These are not polished but they will be useful moving forward and should be easy to troubleshoot.

--- board.cpp
+++ board.cpp
@@ -2,17 +2,62 @@
 
 board::board()
 {
-
+ string finalString = "A B C D E F G H I J\n";
+    for(int i=0;i<10;i++)
+    {
+        finalString += to_string(i + 1) + " ";
+        for(int j=0;j<10;j++)
+        {
+            finalString += m_shipGrid[i][j] + " ";
+        }
+        finalString += "\n";
+    }
+    return finalString;
 }
 //returns a string of the ship board to be outputted
 //this will be used in the beginning of the game when the user is placing ships
 std::string board::drawShipBoard()
 {
+ string finalString = "A B C D E F G H I J\n";
+    for(int i=0;i<10;i++)
+    {
+        finalString += to_string(i + 1) + " ";
+        for(int j=0;j<10;j++)
+        {
+            finalString += m_shipGrid[i][j] + " ";
+        }
+        finalString += "\n";
+    }
+    return finalString;  
 //concatenate strings with "\n" to a create a block string
 }
 //returns a string of both boards to be printed
 //this will be used once the game begins
 std::string board::drawFullBoard()
 {
+string finalString = "A B C D E F G H I J\n";
+    for(int i=0;i<10;i++)
+    {
+        finalString += to_string(i + 1) + " ";
+        for(int j=0;j<10;j++)
+        {
+            finalString += m_shipGrid[i][j] + " ";
+        }
+        finalString += "\n";
+    }
+    return finalString;
 
+    cout << '\n\n=========================\n\n';
+
+    string shotstring = "A B C D E F G H I J\n";
+    for(int i=0;i<10;i++)
+    {
+        shotstring += to_string(i + 1) + " ";
+        for(int j=0;j<10;j++)
+        {
+            shotstring += m_shotGrid[i][j] + " ";
+        }
+        shotstring += "\n";
+    }
+    return shotstring;
 }
