dabdaa83eafdc322c261c28e092b2a652af74d89
kcwidman <kcwidman@gmail.com>
2021-02-19 12:32:44 -0600

    Completed updatePlayerShotAt, hitCheck, and updateShip

--- player.cpp
+++ player.cpp
@@ -36,17 +36,31 @@ bool player::shoot(int x, int y)
 //when the opponent attacks, this function updates the ship grid of the player that is being attacked
 std::string player::updatePlayerShotAt(int x, int y)
 {
-
+    if(hitCheck(x,y) == true)
+    {
+        m_board.m_shipGrid[x][y] = '!';//update ship grid
+        int temp = updateShips(x,y);//update ships returns an int if a ship is destroyed
+        if(temp != 0)
+        {
+            return ("HIT! Ship of size " + std::to_string(temp) + " destroyed!");
+        }
+        else return "**HIT**";
+    }
+    else return "**Miss**";
 }
 //returns true if hit, false otherwise
 bool player::hitCheck(int x, int y)
 {
-
+    if(m_board.m_shipGrid[x][y] == 'o') return true;
+    else return false;
 }
 //updates the ships if any have been hit
-void updateShips()
+int player::updateShips(int x, int y)
 {
-
+    ship s = shipIdentifier(x,y);
+    s.incrementShipHitCount();
+    if(s.sinkCheck() == true) return s.getSize();
+    else return 0;
 }
 //returns the ship that occupies the coordinates (x,y)
 ship player::shipIdentifier(int x, int y)
