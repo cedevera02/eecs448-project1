d2207dda8b74f26237b2108ecf73222ece7c5728
kcwidman <kcwidman@gmail.com>
2021-02-18 18:06:21 -0600

    polished off board.cpp

--- board.cpp
+++ board.cpp
@@ -1,70 +1,64 @@
 #include"board.h"
+using namespace std;
 
 board::board()
 {
-
     for(int i = 0; i < 10; i++)
     {
         for (int j = 0; j < 10; j++)
         {
-            m_shipGrid[i][j] = ".";
-            m_shotGrid[i][j] = ".";
+            m_shipGrid[i][j] = '.';
+            m_shotGrid[i][j] = '.';
         }
     }
-
-
 }
 //returns a string of the ship board to be outputted
 //this will be used in the beginning of the game when the user is placing ships
 std::string board::drawShipBoard()
 {
-
-    string finalString = "A B C D E F G H I J\n";
-
+    std::string finalString = "   A B C D E F G H I J\n";
     for(int i=0;i<10;i++)
     {
-        finalString += to_string(i + 1) + " ";
+        finalString += std::to_string(i + 1) + " ";
+        if(i != 9) finalString += " ";//this keeps the "10" formatted since it's 2 digits
         for(int j=0;j<10;j++)
         {
-            finalString += m_shipGrid[i][j] + " ";
+            finalString += (m_shipGrid[i][j] + std::string(" "));
         }
         finalString += "\n";
     }
-
     return finalString;
-
+}
 //concatenate strings with "\n" to a create a block string
 //returns a string of both boards to be printed
 //this will be used once the game begins
 std::string board::drawFullBoard()
 {
-
-    string finalString = "A B C D E F G H I J\n";
-
+    std::string finalString = "   A B C D E F G H I J\n";
     for(int i=0;i<10;i++)
     {
-        finalString += to_string(i + 1) + " ";
+        finalString += std::to_string(i + 1) + " ";
+        if(i != 9) finalString += " ";//this keeps the "10" formatted since it's 2 digits
         for(int j=0;j<10;j++)
         {
-            finalString += m_shipGrid[i][j] + " ";
+            finalString += (m_shipGrid[i][j] + std::string(" "));
         }
         finalString += "\n";
     }
-    return finalString;
 
+    finalString +='\n\n=========================\n\n';
 
-    cout << '\n\n=========================\n\n';
-
-
-    string shotstring = "A B C D E F G H I J\n";
+    std::string finalString = "   A B C D E F G H I J\n";
     for(int i=0;i<10;i++)
     {
-        shotstring += to_string(i + 1) + " ";
+        finalString += std::to_string(i + 1) + " ";
+        if(i != 9) finalString += " ";//this keeps the "10" formatted since it's 2 digits
         for(int j=0;j<10;j++)
         {
-            shotstring += m_shotGrid[i][j] + " ";
+            finalString += (m_shotGrid[i][j] + std::string(" "));
         }
-        shotstring += "\n";
+        finalString += "\n";
     }
-    return shotstring;
+
+    return finalString;
 }
