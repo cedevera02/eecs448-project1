b9971eb574b84d6520ab226bad511a29752e2694
Poooooe <78933724+Poooooe@users.noreply.github.com>
2021-03-02 15:36:16 -0600

    Adding bool useMissile to check for player input

--- player.h
+++ player.h
@@ -21,10 +21,10 @@ class player
     player(std::string name, int shipCount);
     ~player();
     bool buildAndPlaceShip(int size, bool orien, int xLoc, int yLoc);//returns true and creates new ship under m_ships on valid input, returns false and does nothing on invalid input
-    void playerTurn(int x, int y, bool hitCheck);//this function updates the player's shot grid
+    void playerTurn(int x, int y, bool hitCheck, bool useMissile);//this function updates the player's shot grid
         bool shoot(int x, int y);//returns true for valid input, false otherwise
-    std::string updatePlayerShotAt(int x, int y);//returns "**HIT!**", "**Miss**", "Ship of size <s> destroyed!", updates the ships and ship board when the opponent fires
-        bool hitCheck(int x, int y);//returns true if hit, false otherwise, this is called when the opponent attacks
+    std::string updatePlayerShotAt(int x, int y, bool useMissile);//returns "**HIT!**", "**Miss**", "Ship of size <s> destroyed!", updates the ships and ship board when the opponent fires
+        bool hitCheck(int x, int y, bool useMissile);//returns true if hit, false otherwise, this is called when the opponent attacks
         int updateShip(int x, int y);//if the shot is a hit, updates the ships and return the ship size if ship is sunk, returns 0 if not sunk
             ship* shipIdentifier(int x, int y);//returns the ship that occupies the coordinates (x,y)
     bool loserCheck();//retuns true if the player loses (shipCount == sinkCount), false otherwise
