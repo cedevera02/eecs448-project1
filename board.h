03336dd5cd09c37ac9e094e8ca0e8ae19d7a14a2
josiezxh <josiejharding@gmail.com>
2021-03-12 20:03:06 -0600

    code documenting for board

--- board.h
+++ board.h
@@ -11,7 +11,7 @@ class board
     public:
     char m_shotGrid[10][10];///< '.' - empty spot, '0' - miss, 'X' - hit
     char m_shipGrid[10][10];///< '.' - emtpty spot, 'o' - ship, '!' - hit ship
-    board();
+    board();///< board constructor, sets up 2D arrays as the boards
     std::string drawShipBoard();///< returns a string of the ship board to be outputted, this will be used when the user is placing ships
     std::string drawFullBoard();///< returns a string of both boards to be printed
 
